f591b816b2349b0db2682029d789039853262913
Sahasragoud <sahasramuthyala04@gmail.com>
2025-08-26 22:22:52 +0530

    defined models

--- smart_waste/core/models.py
+++ smart_waste/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Waste Categories
+class WasteCategory(models.TextChoices):
+    BIODEGRADABLE = 'biodegradable', 'Biodegradable'
+    RECYCLABLE = 'recyclable', 'Recyclable'
+    HAZARDOUS = 'hazardous', 'Hazardous'
+
+# Waste Item
+class WasteItem(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=WasteCategory.choices)
+    image = models.ImageField(upload_to='waste_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# User Points (needed for Feature 4)
+class UserPoints(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
