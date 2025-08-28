f960aded4ef09af2d671705e8e8d9539bae645ec
Sahasragoud <sahasramuthyala04@gmail.com>
2025-08-28 13:06:49 +0530

    Cleared repository

--- smart_waste/core/ml_model.py
+++
@@ -1,7 +0,0 @@
-import os
-from keras.models import load_model
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_PATH = os.path.join(BASE_DIR, 'model', 'waste_classifier_model.h5')
-
-model = load_model(MODEL_PATH)
